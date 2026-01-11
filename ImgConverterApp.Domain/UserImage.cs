17182c02a703789b84a79456a13713867223a161
optiMISTAKE <mt313672@student.polsl.pl>
2026-01-11 13:40:29 +0100

    Add project files.

---
+++ ImgConverterApp.Domain/UserImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgConverterApp.Domain
+{
+    public class UserImage
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public string UserId { get; private set; }
+        public string OriginalFileName { get; private set; }
+        public string StoredFileName { get; private set; }
+        public string StoredPath { get; private set; }
+        public ImageFormat Format { get; private set; }
+        public long SizeInBytes { get; private set; }
+        
+        // date and time when the image was created
+        public DateTime CreatedAt { get; private set; }
+        // after 30 days the image will be deleted
+        public DateTime ExpiresAt => CreatedAt.AddDays(30);
+
+        // constructor
+        public UserImage(string userId, string originalFileName, string storedFileName, string filePath, long sizeInBytes, ImageFormat format, DateTime createdAt)
+        {
+            // Optional: Add simple validation (Domain Guardrails)
+            if (string.IsNullOrWhiteSpace(userId)) throw new ArgumentException("User is required");
+            if (sizeInBytes <= 0) throw new ArgumentException("File cannot be empty");
+
+            UserId = userId;
+            OriginalFileName = originalFileName;
+            StoredFileName = storedFileName;
+            StoredPath = filePath;
+            SizeInBytes = sizeInBytes;
+            Format = format;
+            CreatedAt = createdAt;
+        }
+    }
+}
---
+++ ImgConverterApp.Infrastructure/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImgConverterApp.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImgConverterApp.Infrastructure
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet for UserImage entities
+        public DbSet<UserImage> UserImages { get; set; }
+
+        // configure the model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserImage>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.UserId).IsRequired();
+                entity.Property(e => e.OriginalFileName).IsRequired();
+                entity.Property(e => e.StoredFileName).IsRequired();
+                entity.Property(e => e.StoredPath).IsRequired();
+                entity.Property(e => e.Format).IsRequired();
+                entity.Property(e => e.SizeInBytes).IsRequired();
+                entity.Property(e => e.CreatedAt).IsRequired();
+
+                // we ignore ExpiresAt as it's a computed property
+                entity.Ignore(e => e.ExpiresAt);
+            });
+            // call the base method
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
